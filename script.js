7197a0d6c1a193b3efe00f71aaa46f15473f6cbd
Pawel <pawel.werbowy@gmail.com>
2024-04-29 13:25:23 +0100

    adding structure

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ let incomes = [
   { name: "Tax Return", amount: 750, recurring: false },
   { name: "Passive Income", amount: 200, recurring: false },
 ];
+
+let expenses = [
+  { name: "Rent", amount: 800, recurring: true },
+  { name: "Food", amount: 400, recurring: true },
+  { name: "Utilities", amount: 100, recurring: true },
+  { name: "Car Insurance", amount: 200, recurring: false },
+  { name: "Car Repair", amount: 700, recurring: false },
+];
