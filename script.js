9e05e4d08b567a37aa1eccd99bdce0faaa4293c1
Pawel <pawel.werbowy@gmail.com>
2024-04-29 13:19:29 +0100

    add files

---
+++ script.js
@@ -0,0 +1,7 @@
+let incomes = [
+  { name: "Salary", amount: 2000, recurring: true },
+  { name: "Side Hustle", amount: 500, recurring: true },
+  { name: "Bonus", amount: 1000, recurring: false },
+  { name: "Tax Return", amount: 750, recurring: false },
+  { name: "Passive Income", amount: 200, recurring: false },
+];
